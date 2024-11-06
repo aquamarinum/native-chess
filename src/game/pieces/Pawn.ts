b0ac68db98a1da6ef2ed43d7b454d72523ac1ab6
Artemy-sweden <ghosteagle2020@mail.ru>
2024-11-06 22:48:44 +0300

    feat: pawn movement highlighting NO EN-PASSANT and toQueen transformgit add .!

--- src/game/pieces/Pawn.ts
+++ src/game/pieces/Pawn.ts
@@ -14,7 +14,75 @@ export class Pawn extends ChessPiece {
         : ViewModels.PAWN_WHITE,
     );
   }
-  public highlight(currentPos: CellPositionType, matrix: ChessBoard): void {
-    //HIGHLIGHT LIKE PAWN
+  public highlight(currentPos: CellPositionType, board: ChessBoard): void {
+    ///FOR WHITE
+    if (this.color === ChessColors.WHITE) {
+      const newPosition = currentPos;
+
+      //LONG JUMP
+      if (currentPos.y === 6) {
+        board.checkPosition({y: currentPos.y - 2, x: currentPos.x});
+      }
+
+      //DEFAULT JUMP
+      newPosition.y--;
+      if (newPosition.y >= 0) {
+        board.checkPosition(newPosition);
+
+        //CAPTURE LEFT
+        newPosition.x--;
+        if (
+          newPosition.x >= 0 &&
+          board.getPositionAt(newPosition).piece?.color !== this.color
+        )
+          board.checkPosition(newPosition);
+
+        //CAPTURE RIGHT
+        newPosition.x += 2;
+        if (
+          newPosition.x < 8 &&
+          board.getPositionAt(newPosition).piece?.color !== this.color
+        )
+          board.checkPosition(newPosition);
+      }
+
+      //EN PASSANT
+      //QUEEN TRANSFORMING
+    }
+
+    ///FOR BLACK
+    else {
+      const newPosition = currentPos;
+
+      //LONG JUMP
+      if (currentPos.y === 1) {
+        board.checkPosition({y: currentPos.y + 2, x: currentPos.x});
+      }
+
+      //DEFAULT JUMP
+      newPosition.y++;
+      if (newPosition.y < 8) {
+        board.checkPosition(newPosition);
+
+        //CAPTURE LEFT
+        newPosition.x--;
+        if (
+          newPosition.x >= 0 &&
+          board.getPositionAt(newPosition).piece?.color !== this.color
+        )
+          board.checkPosition(newPosition);
+
+        //CAPTURE RIGHT
+        newPosition.x += 2;
+        if (
+          newPosition.x < 8 &&
+          board.getPositionAt(newPosition).piece?.color !== this.color
+        )
+          board.checkPosition(newPosition);
+      }
+
+      //EN PASSANT
+      //QUEEN TRANSFORMING
+    }
   }
 }
