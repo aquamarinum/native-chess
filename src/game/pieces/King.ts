ad4592077205ef9d0357d1877573f07d379a670d
Artemy-sweden <ghosteagle2020@mail.ru>
2024-11-06 22:58:42 +0300

    feat: king stupid step movement highlighting

--- src/game/pieces/King.ts
+++ src/game/pieces/King.ts
@@ -14,7 +14,26 @@ export class King extends ChessPiece {
         : ViewModels.KING_WHITE,
     );
   }
-  public highlight(currentPos: CellPositionType, matrix: ChessBoard): void {
-    //HIGHLIGHT LIKE KING
+  public highlight(currentPos: CellPositionType, board: ChessBoard): void {
+    if (currentPos.y > 0) {
+      board.checkPosition({y: currentPos.y - 1, x: currentPos.x});
+      if (currentPos.x > 0)
+        board.checkPosition({y: currentPos.y - 1, x: currentPos.x - 1});
+      if (currentPos.x < 7)
+        board.checkPosition({y: currentPos.y - 1, x: currentPos.x + 1});
+    }
+    if (currentPos.y < 7) {
+      board.checkPosition({y: currentPos.y + 1, x: currentPos.x});
+      if (currentPos.x > 0)
+        board.checkPosition({y: currentPos.y + 1, x: currentPos.x - 1});
+      if (currentPos.x < 7)
+        board.checkPosition({y: currentPos.y + 1, x: currentPos.x + 1});
+    }
+    if (currentPos.x > 0) {
+      board.checkPosition({y: currentPos.y, x: currentPos.x - 1});
+    }
+    if (currentPos.x < 7) {
+      board.checkPosition({y: currentPos.y, x: currentPos.x + 1});
+    }
   }
 }
